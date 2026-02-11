018fbd254e5f396bcf8314d92f9373588d63e1e6
chelseadopamin <146686534+CHELSEADOPAMIN@users.noreply.github.com>
2026-02-11 22:02:34 +1100

    feat: define todo types

---
+++ nimbly-todo-list/lib/types/todo.ts
@@ -0,0 +1,26 @@
+export interface Todo {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+}
+
+export interface TodosResponse {
+  todos: Todo[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface CreateTodoRequest {
+  todo: string;
+  completed: boolean;
+  userId: number;
+}
+
+export interface UpdateTodoRequest {
+  todo?: string;
+  completed?: boolean;
+}
+
+export type TodoView = "today" | "history";
